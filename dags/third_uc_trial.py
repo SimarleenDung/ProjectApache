b332145a4cda99f42be0d709b749e6ca8d99bc2d
SimarleenDung <simudung@gmail.com>
2026-01-02 17:53:12 +0530

    Removed the email code snippets that were commented

--- dags/third_uc_trial.py
+++ dags/third_uc_trial.py
@@ -5,95 +5,11 @@ from datetime import datetime
 from airflow.utils.task_group import TaskGroup
 from airflow.models import Variable
 import os
-#from airflow.operators.email import EmailOperator          
-#from airflow.operators.python import PythonOperator        
-#from airflow.utils.db import provide_session
-#from airflow.models import XCom
  
 from utils.parquet_utils import csv_to_parquet
 from utils.snowflake_utils import truncate_and_load
 from utils.file_discovery_utils import get_all_new_files, update_processed_files
 from utils.file_router_utils import resolve_table_from_filename
-
-# Read alert email ONCE at top level 
-#ALERT_EMAIL = Variable.get("alert_email")
-
-# Email + XCom failure callbacks
-
-#def on_task_failure_callback(context):
-    #"""Collect info of failed tasks in XCom"""
-    #ti = context['ti']
-    #dag_name = context['dag'].dag_id
-    #run_id = dag_name
-
-    #print(f"Task {ti.task_id} failed. Saving info to XCom.")
-
-    #ti.xcom_push(
-        #key=f"failed_task_info_{run_id}",
-        #value={
-           # 'dag_id': dag_name,
-           # 'task_id': ti.task_id,
-           # 'log_url': ti.log_url
-        #}
-    #)
-
-
-#@provide_session
-#def on_dag_failure_callback(session=None, **context):
-    #"""Aggregate failed tasks from XCom and send Gmail email"""
-    #dag_name = context['dag'].dag_id
-    #run_id = dag_name
-    #execution_date = context['execution_date']
-   # email_recipients = [Variable.get("alert_email")]
-
-    # Query failed tasks from XCom
-    #failed_tasks = session.query(XCom).filter(
-      #  XCom.dag_id == dag_name,
-        #XCom.key == f"failed_task_info_{run_id}",
-       # XCom.execution_date == execution_date
-   # ).all()
-
-    #print(f"Found {len(failed_tasks)} failed tasks for DAG {dag_name}.")
-
-   # if not failed_tasks:
-        #return
-
-    # Construct HTML message
-    ##msg = """
-#<h3>The following tasks failed in the DAG:</h3>
-#<table border="1" style="border-collapse: collapse; width: 100%;">
-#<tr>
-#<th>DAG ID</th>
-#<th>Task ID</th>
-#<th>Log Link</th>
-#</tr>
-#"""
- #   for task in failed_tasks:
-  #      task_info = task.value
-   #     msg += f"""
-#<tr>
-#<td>{task_info['dag_id']}</td>
-#<td>{task_info['task_id']}</td>
-#<td><a href="{task_info['log_url']}">Log Link</a></td>
-#</tr>
-#"""
- #   msg += "</table>"
-
-    # Send Gmail email
-  #  email = EmailOperator(
-   #     task_id="failure_notification",
-    #    to=email_recipients,
-     #   subject=f"DAG Failure Notification for {dag_name}",
-      #  html_content=msg,
-       # conn_id="smtp_gmail"   
-    #)
-    #email.execute(context=context)
-
-    # Cleanup XCom entries
-    #session.query(XCom).filter(
-    #    XCom.dag_id == dag_name,
-    #    XCom.key == f"failed_task_info_{run_id}"
-    #).delete()
 
 # Schema change detection task
 @task
@@ -133,25 +49,6 @@ def detect_schema_change(files):
             Variable.set(ref_schema_var, ",".join(new_schema))  # update stored schema
 
     return files_with_schema_change
-
-# Email sender for schema change
-#def send_schema_change_email(files_with_change, **context):
-   # if not files_with_change:
-       # return
-    #email_recipients = [Variable.get("alert_email")]
-    #file_list_html = "<br>".join(files_with_change)
-
-   # email = EmailOperator(
-       # task_id="schema_change_email",
-        #to=email_recipients,
-        #subject="Schema Change Detected in Incoming Files",
-       # html_content=f"""
-       # <h3>Schema change detected in the following files:</h3>
-       # {file_list_html}
-       # """,
-       # conn_id="smtp_gmail"
-   #)
-   # email.execute(context=context)
 
 with DAG(
     dag_id="third_uc_trial",
@@ -193,21 +90,6 @@ with DAG(
         return valid_files
    
     new_files_list = discover_new_files()
- 
-    # Detect schema changes
-    #schema_changed_files = detect_schema_change(new_files_list)
-
-    # Send email if schema changed
-    #@task
-    #def send_schema_change_email_task(files_with_change):
-       # if files_with_change:
-          #  send_schema_change_email(files_with_change)
-       # else:
-           # print("No schema changes detected — skipping email.")
-        #return "done"
-
-    #send_schema_change_task = send_schema_change_email_task(schema_changed_files)
-
 
     with TaskGroup(group_id="convert_files") as convert_files:
         # CSV → Parquet (parallel and safely)
