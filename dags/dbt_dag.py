c45da5bdce7af7cd84fc804348a14c1b789d7b73
SimarleenDung <simudung@gmail.com>
2026-02-02 15:56:26 +0530

    Added the use of a Flag. The original code remains commented.

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -1,18 +1,60 @@
+# from airflow import DAG
+# from airflow.operators.bash import BashOperator
+# from datetime import datetime
+ 
+# with DAG(
+#     dag_id="dbt_single_model",
+#     start_date=datetime(2026, 1, 22),
+#     schedule_interval=None,
+#     catchup=False,
+# ) as dag:
+ 
+#     run_one_model = BashOperator(
+#         task_id="run_my_model",
+#         bash_command="""
+#         cd /opt/airflow/demo_dbt/dbt_project && dbt run \
+#         --select trialmodel \
+#         --profiles-dir /opt/airflow/demo_dbt/dbt_project"""
+#     )
+
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
- 
+import json
+
+def create_dbt_command(selected_customer=None):
+    base_cmd = (
+        "cd /opt/airflow/demo_dbt/dbt_project && "
+        "dbt run --select trialmodel "
+        "--profiles-dir /opt/airflow/demo_dbt/dbt_project"
+    )
+
+    if selected_customer and selected_customer != "None":
+        vars_json = json.dumps(
+            {"selected_customer": selected_customer}
+        ).replace('"', '\\"')
+        base_cmd += f' --vars "{vars_json}"'
+
+    return base_cmd
+
 with DAG(
-    dag_id="dbt_single_model",
+    dag_id="dbt_single_flag",
     start_date=datetime(2026, 1, 22),
     schedule_interval=None,
     catchup=False,
 ) as dag:
- 
-    run_one_model = BashOperator(
-        task_id="run_my_model",
-        bash_command="""
-        cd /opt/airflow/demo_dbt/dbt_project && dbt run \
-        --select trialmodel \
-        --profiles-dir /opt/airflow/demo_dbt/dbt_project"""
+
+    Start = EmptyOperator(task_id="Start")
+    End = EmptyOperator(task_id="End")
+
+    run_dbt = BashOperator(
+        task_id="run_trialmodel",
+        bash_command=create_dbt_command(
+            selected_customer="{{ dag_run.conf.get('selected_customer') }}"
+        )
     )
+
+    # Dependencies
+    Start >> run_dbt >> End
--- demo_dbt/dbt_project/models/trialmodel.sql
+++ demo_dbt/dbt_project/models/trialmodel.sql
@@ -1,7 +1,35 @@
+/*{{ config(
+    materialized = 'table',
+    schema = 'DBT_SDUNG',
+    database = 'AIRFLOW_DB'
+) }}
+
+select * from {{source('trialsource','CUSTOMERS_SCD_TEST')}}*/
+
 {{ config(
     materialized = 'table',
     schema = 'DBT_SDUNG',
     database = 'AIRFLOW_DB'
 ) }}
 
-select * from {{source('trialsource','CUSTOMERS_SCD_TEST')}}
+{% set selected_customer = var('selected_customer', 'all') %}
+
+-- Simple model with 1 flag demonstrated
+
+with source_data as (
+    select * 
+    from {{ source('trialsource','CUSTOMERS_SCD_TEST') }}
+),
+
+filtered_data as (
+    select *
+    from source_data
+    where 1=1
+    {% if selected_customer not in ['all', 'None', none] %}
+        and email = '{{ selected_customer }}'
+    {% endif %}
+)
+
+select *
+from filtered_data
+    
